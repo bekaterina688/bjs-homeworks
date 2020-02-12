1282cef95957ba5cc9f7b9c46e034f4d9baafe19
Ekaterina Bogdanova <bekaterina688@gmail.com>
2020-02-12 17:57:24 +0300

    1 задача

--- 1.2-extended-syntax/task.js
+++ 1.2-extended-syntax/task.js
@@ -1,6 +1,17 @@
 function getResult(a,b,c){
-    // код для задачи №1 писать здесь
-    // return x;
+    let x;
+	let discriminant = Math.pow(b,2) - 4 * a * c;
+	 console.log(discriminant);
+	
+	if (discriminant < 0) {
+		return [];
+	} else if (discriminant == 0) {
+		x = [-b / (2 * a)];
+		return x;
+	} else {
+		x = [((-1 * b) + Math.sqrt(discriminant)) / (2 * a) , ((-1 * b) - Math.sqrt(discriminant)) / (2 * a)];
+		return x;
+	}
 }
 
 function getAverageMark(marks){
